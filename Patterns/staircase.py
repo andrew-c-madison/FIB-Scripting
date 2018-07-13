fc3b7bd60679dea7e529220d189d332723c0ae80
andrew-c-madison <38506056+andrew-c-madison@users.noreply.github.com>
2018-07-13 11:11:35 -0400

    Add files via upload

--- Patterns/staircase.py
+++ Patterns/staircase.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 
 
-# Get arguments from command line
+# Get input from command line
 parser = argparse.ArgumentParser(description="Creates a staircase pattern")
 parser.add_argument("-n", "--num_steps", type=int, default=128, help="Number of steps")
 args = parser.parse_args()
@@ -15,18 +15,23 @@ n = args.num_steps
 size = (4096, 4096)
 w = size[0]/n
 dv = 256/n
-val = 0
 
 
-# Create staircase image
+# Create image
 img = np.zeros(size)
+val = 0
 for i,row in enumerate(img):
     img[i, :] = val*np.ones(row.shape)
     if i%w == 0:
         val += dv
 
+
 # Save image
 img = np.dstack((img, img, img))
 img = Image.fromarray(np.uint8(img))
-img.save('staircase.png')
+img_dir = "Images"
+if not os.path.exists(img_dir): os.makedirs(img_dir)
+fname = os.path.join(img_dir, "staircase_{}_steps.png".format(n))
+img.save(fname)
+print "Pattern saved as {}.".format(fname)
 img.show()
